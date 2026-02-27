a09a8279d49f0670b7c29f3b191271470660da76
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-02-27 22:08:42 +0600

    Done swap with 3 methods

--- class-01/index.js
+++ class-01/index.js
@@ -1,16 +1,53 @@
 // Type Coercion
 // JavaScript-এ Type Coercion হলো এমন একটি প্রক্রিয়া যেখানে একটি data type স্বয়ংক্রিয়ভাবে বা ইচ্ছাকৃতভাবে অন্য একটি data type-এ রূপান্তরিত হয়।
 
-let a = 10
-let b = "5"
+// let a = 10
+// let b = "5"
 
 // console.log(a + b)
 // console.log(a - b)
 
-// =========================================================================
+// =======================================================================================================
 
 // Type Conversion / Type Casting
 // JavaScript-এ Type Conversion / Type Casting হলো এমন একটি প্রক্রিয়া যেখানে একটি data type-কে ইচ্ছাকৃতভাবে (manually) অন্য একটি data type-এ রূপান্তর করা হয়।
 
 // let age = Number(prompt("Enter your age"));
 // console.log(age)
+
+// =======================================================================================================
+
+// Swap
+// Programming-এ Swap হলো এমন একটি প্রক্রিয়া যেখানে দুটি ভেরিয়েবলের মান (value) একে অপরের সাথে অদল-বদল করা হয়।
+
+// 01. Swap with extra variable
+
+// let a = 10
+// let b = 20
+// let c;
+
+// c = a // 10
+// a = b // 20
+// b = c // 10
+
+// console.log(a, b)
+
+// 02. Swap without extra variable
+
+// let a = 10
+// let b = 20
+
+// a = a + b // 10 + 20 = 30
+// b = a - b // 30 - 20 = 10
+// a = a - b // 30 - 10 = 20
+
+// console.log(a, b)
+
+// 03. Swap with destructuring
+
+let a = 10;
+let b = 20;
+
+[a, b] = [b, a];
+
+console.log(a, b);
