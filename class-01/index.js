57bafbd6a06cff766fcd2bb19b2e6cc5c4fb2412
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-02-27 20:43:31 +0600

    Done Type coercion and type conversion

---
+++ class-01/index.js
@@ -0,0 +1,16 @@
+// Type Coercion
+// JavaScript-এ Type Coercion হলো এমন একটি প্রক্রিয়া যেখানে একটি data type স্বয়ংক্রিয়ভাবে বা ইচ্ছাকৃতভাবে অন্য একটি data type-এ রূপান্তরিত হয়।
+
+let a = 10
+let b = "5"
+
+// console.log(a + b)
+// console.log(a - b)
+
+// =========================================================================
+
+// Type Conversion / Type Casting
+// JavaScript-এ Type Conversion / Type Casting হলো এমন একটি প্রক্রিয়া যেখানে একটি data type-কে ইচ্ছাকৃতভাবে (manually) অন্য একটি data type-এ রূপান্তর করা হয়।
+
+// let age = Number(prompt("Enter your age"));
+// console.log(age)
