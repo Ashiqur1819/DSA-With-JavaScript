49250ba874a43b03cf0cf51afa01e88e4dec9124
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-03-03 19:03:33 +0600

    Done prime number problem

--- class-03/index.js
+++ class-03/index.js
@@ -1,4 +1,36 @@
 // Loop and Recursion
+
+// Factors problem
+
+// const pr = prompt("Enter your number: ");
+
+// if (pr === null) {
+//   console.log("Cancelled");
+// } else {
+//   const n = Number(pr);
+
+//   if (!Number.isInteger(n) || n <= 0) {
+//     console.log("Enter a valid positive integer");
+//   } else {
+//     const divisors = [];
+
+//     // 36
+
+//     for (let i = 1; i <= Math.sqrt(n); i++) {
+//       if (n % i === 0) {
+//         divisors.push(i);
+//         if (i !== n / i) {
+//           divisors.push(n / i);
+//         }
+//       }
+//     }
+
+//     divisors.sort((a, b) => a - b);
+//     console.log(divisors);
+//   }
+// }
+
+// Prime number
 const pr = prompt("Enter your number: ");
 
 if (pr === null) {
@@ -9,20 +41,15 @@ if (pr === null) {
   if (!Number.isInteger(n) || n <= 0) {
     console.log("Enter a valid positive integer");
   } else {
-    const divisors = [];
 
-    // 36
+    let isPrime = true;
 
-    for (let i = 1; i <= Math.sqrt(n); i++) {
-      if (n % i === 0) {
-        divisors.push(i);
-        if (i !== n / i) {
-          divisors.push(n / i);
+    for(let i = 2; i <= Math.floor(n / 2); i++){
+        if(n % i === 0){
+            isPrime = false
         }
-      }
     }
 
-    divisors.sort((a, b) => a - b);
-    console.log(divisors);
+    console.log(isPrime)
   }
 }
